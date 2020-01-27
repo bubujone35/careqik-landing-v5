d35eccf26d58e77c74910cf35226ac0a251daabc
hvnwntw8 <ryan.m.lee89@gmail.com>
2020-01-26 17:08:11 -0700

    trying a basic form on root

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,23 @@ const IndexPage = () => (
     <Posts />
     <Subscribe />
     <Floating />
+    <div>
+      <h3>Secure Form</h3>
+      <form name="contact" method="post" netlify-honeypot="b" autocomplete="off" data-netlify data-secure>
+        <p>
+          <label for="name">Basic Field</label>
+          <input type="text" name="name" id="name" />
+        </p>
+
+        <p>
+          <label for="secure">Secure Field</label>
+          <input type="text" name="secure" id="secure" data-secure-field />
+        </p>
+        <p>
+          <button type="submit" id="submit">Send</button>
+        </p>
+      </form>
+    </div>
   </Layout>
 )
 
