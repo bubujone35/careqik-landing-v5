d147a856b957dbd0569a306df516ef0d943a0d92
Chris <Chris@Chriss-Air.home>
2020-02-24 22:49:47 -0500

    added missing data-secure attribute to contact page / contact form component

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { navigate } from 'gatsby-link'
+import { navigate } from "gatsby-link"
 import styled from "styled-components"
 import PrimaryButton from "../components/buttons/primary"
 
@@ -27,7 +27,6 @@ const StyledInput = styled.input`
   box-sizing: border-box;
   border: 1px solid rgba(51, 132, 154, 0.95);
   border-radius: 4px;
-
 `
 const StyledText = styled.textarea`
   width: 100%;
@@ -39,74 +38,74 @@ const StyledText = styled.textarea`
   height: 150px;
 `
 
-
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
 }
 
 export default function Contact() {
   const [state, setState] = React.useState({})
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault()
     const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
+        "form-name": form.getAttribute("name"),
         ...state,
       }),
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
   }
 
-return (
-  <Layout>
-    <SEO title="Contact" />
-    <Wrapper>
-    <Title>Contact</Title>
-      <form
-        name="contact"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        <p>
-          <StyledLabel>
-            First name
-            <br />
-            <StyledInput type="text" name="name" onChange={handleChange} />
-          </StyledLabel>
-        </p>
-        <p>
-          <StyledLabel>
-            Email
-            <br />
-            <StyledInput type="email" name="email" onChange={handleChange} />
-          </StyledLabel>
-        </p>
-        <p>
-          <StyledLabel>
-            Message
-            <br />
-            <StyledText name="message" onChange={handleChange} />
-          </StyledLabel>
-        </p>
+  return (
+    <Layout>
+      <SEO title="Contact" />
+      <Wrapper>
+        <Title>Contact</Title>
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          data-secure
+          onSubmit={handleSubmit}
+        >
+          <p>
+            <StyledLabel>
+              First name
+              <br />
+              <StyledInput type="text" name="name" onChange={handleChange} />
+            </StyledLabel>
+          </p>
+          <p>
+            <StyledLabel>
+              Email
+              <br />
+              <StyledInput type="email" name="email" onChange={handleChange} />
+            </StyledLabel>
+          </p>
+          <p>
+            <StyledLabel>
+              Message
+              <br />
+              <StyledText name="message" onChange={handleChange} />
+            </StyledLabel>
+          </p>
 
-        <p>
-          <PrimaryButton type="submit">Submit</PrimaryButton>
-        </p>
-      </form>
+          <p>
+            <PrimaryButton type="submit">Submit</PrimaryButton>
+          </p>
+        </form>
       </Wrapper>
-  </Layout>
-)
+    </Layout>
+  )
 }
